9cb3d1b7dc17dc86dbcc1d952a1fdae4268daaf3
Brendan Rogers <brendanr11@gmail.com>
2018-02-22 01:18:51 -0500

    Added all functionality. HTML, CSS, and JS fully functional.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 // Javascript File
+
+// Self executing Anonymous function (SAF)
+// avoid global variables
+(function(){ 
+
+// Check in
+console.log("Beep boop. Connected to Mainframe.");
+
+// VARIABLES
+var iconSet = document.querySelectorAll('svg');
+
+// FUNCTIONS
+function logThisIcon() {
+	console.log('clicked on this icon', this.id);
+	this.style.WebkitTransform = "rotate(180deg)"; 
+	this.style.transform = "rotate(180deg)"; 
+}
+
+
+iconSet.forEach(icon => icon.addEventListener('click', logThisIcon));
+
+
+}) ();
